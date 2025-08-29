4d80bcd7ffc3f6114fbf60c83df3ce3f55f5f04f
Michael Greaves <michael.greaves@epidemicsound.com>
2025-08-29 16:54:23 +0200

    Add failing test

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,45 @@
+package auth
+
+import (
+	"net/http"
+	"testing"
+	"errors"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	cases := []struct {
+		input          http.Header
+		expected       string
+		expectedErr		 error
+	}{
+		{
+			input: http.Header{
+				"Authorization": []string{"ApiKey 123456"},
+			},
+			expected: "123457",
+			expectedErr: nil,
+		},
+		{
+			input: http.Header{},
+			expected: "",
+			expectedErr: ErrNoAuthHeaderIncluded,
+		},
+	}
+
+	for _, c := range cases {
+		actual, err := GetAPIKey(c.input)
+		if c.expectedErr != nil {
+			if !errors.Is(err, c.expectedErr) {
+				t.Errorf("Expected error %v, but got %v", c.expectedErr, err)
+			}
+			continue
+		}
+
+		if err != nil {
+			t.Errorf("Expected no error but got %v", err)
+		}
+		if actual != c.expected {
+			t.Errorf("Expected: %s, but got %s", c.expected, actual)
+		}
+	}
+}
